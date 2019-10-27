048ae35f982a9f18062c53b767be52bf1685eb0a
aben20807 <aben20807@gmail.com>
2019-10-28 00:21:52 +0800

    Remove the class

--- hw1/app.py
+++ hw1/app.py
@@ -4,7 +4,7 @@ import os
 import sys
 from functools import partial
 
-from problems.problem1 import Problem1
+from problems import problem1
 
 class MainUi(QtWidgets.QMainWindow):
     def __init__(self):
@@ -15,17 +15,17 @@ class MainUi(QtWidgets.QMainWindow):
 
         menubar = self.menuBar()
         close_all = QAction('Close all CV windows', self)
-        close_all.triggered.connect(Problem1.close_all)
+        close_all.triggered.connect(problem1.close_all)
         menubar.addAction(close_all)
 
         self.show()
 
     def bind_buttons(self):
         bind_dict = {
-            'b1_1': Problem1.p1_1,
-            'b1_2': Problem1.p1_2,
-            'b1_3': Problem1.p1_3,
-            'b1_4': Problem1.p1_4,
+            'b1_1': problem1.p1_1,
+            'b1_2': problem1.p1_2,
+            'b1_3': problem1.p1_3,
+            'b1_4': problem1.p1_4,
         }
         for object_name, bind_function in bind_dict.items():
             self.findChild(QtWidgets.QPushButton, object_name).clicked.connect(partial(bind_function, self))
--- hw1/problems/problem1.py
+++ hw1/problems/problem1.py
@@ -2,50 +2,40 @@ import cv2
 import os
 import numpy as np
 
-class Problem1():
-    
-    def __init__(self, ui):
-        self.ui = ui
+def close_all():
+        cv2.destroyAllWindows()
 
-    @staticmethod
-    def close_all():
-         cv2.destroyAllWindows()
+def p1_1(ui):
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-    @staticmethod
-    def p1_1(ui):
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    (chessboard_height, chessboard_width) = (11, 8)
+    objp = np.zeros((chessboard_height*chessboard_width,3), np.float32)
+    objp[:,:2] = np.mgrid[0:chessboard_width,0:chessboard_height].T.reshape(-1,2)
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    for i in range(1, 16):
+        filename = os.getcwd() + os.sep + "images" + os.sep + "images" + os.sep + "CameraCalibration" + os.sep + str(i) + ".bmp"
+        img = cv2.imread(filename)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, (chessboard_height,chessboard_width), None)
+        if ret == True:
+            objpoints.append(objp)
+            corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+            imgpoints.append(corners2)
 
-        (chessboard_height, chessboard_width) = (11, 8)
-        objp = np.zeros((chessboard_height*chessboard_width,3), np.float32)
-        objp[:,:2] = np.mgrid[0:chessboard_width,0:chessboard_height].T.reshape(-1,2)
-        objpoints = [] # 3d point in real world space
-        imgpoints = [] # 2d points in image plane.
-        for i in range(1, 16):
-            filename = os.getcwd() + os.sep + "images" + os.sep + "images" + os.sep + "CameraCalibration" + os.sep + str(i) + ".bmp"
-            img = cv2.imread(filename)
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            ret, corners = cv2.findChessboardCorners(gray, (chessboard_height,chessboard_width), None)
-            if ret == True:
-                objpoints.append(objp)
-                corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-                imgpoints.append(corners2)
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(img, (chessboard_height,chessboard_width), corners2,ret)
+            img = cv2.resize(img, (960,540))
+            cv2.imshow("img" + str(i), img)
 
-                # Draw and display the corners
-                img = cv2.drawChessboardCorners(img, (chessboard_height,chessboard_width), corners2,ret)
-                img = cv2.resize(img, (960,540))
-                cv2.imshow("img" + str(i), img)
+def p1_2(ui):
+    print("ouo")
+    ui.t3_1_angle.setText("OuO")
 
-    @staticmethod
-    def p1_2(ui):
-        print("ouo")
-        ui.t3_1_angle.setText("OuO")
+def p1_3(ui):
+    print("ouo")
+    ui.t3_1_angle.setText("OuO")
 
-    @staticmethod
-    def p1_3(ui):
-        print("ouo")
-        ui.t3_1_angle.setText("OuO")
-
-    @staticmethod
-    def p1_4(ui):
-        print("ouo")
-        ui.t3_1_angle.setText("OuO")
+def p1_4(ui):
+    print("ouo")
+    ui.t3_1_angle.setText("OuO")
