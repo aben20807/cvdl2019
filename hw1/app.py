8cc50a938e4057118f7e3adb2ef42fb192650759
aben20807 <aben20807@gmail.com>
2019-10-27 22:52:24 +0800

    Bind buttons to the functions

--- hw1/app.py
+++ hw1/app.py
@@ -1,13 +1,27 @@
 from PyQt5 import QtWidgets, uic
 import os
 import sys
+from functools import partial
+
+from problems.problem1 import Problem1
 
 class MainUi(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainUi, self).__init__()
         ui_path = os.getcwd() + os.sep + "ui" + os.sep + "hw1.ui"
         uic.loadUi(ui_path, self)
+        self.bind_buttons()
         self.show()
+
+    def bind_buttons(self):
+        bind_dict = {
+            'b1_1': Problem1.p1_1,
+            'b1_2': Problem1.p1_2,
+            'b1_3': Problem1.p1_3,
+            'b1_4': Problem1.p1_4,
+        }
+        for object_name, bind_function in bind_dict.items():
+            self.findChild(QtWidgets.QPushButton, object_name).clicked.connect(partial(bind_function, self))
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
---
+++ hw1/problems/problem1.py
@@ -0,0 +1,24 @@
+class Problem1():
+    
+    def __init__(self, ui):
+        self.ui = ui
+
+    @staticmethod
+    def p1_1(ui):
+        print("ouo")
+        ui.t3_1_angle.setText("OuO")
+
+    @staticmethod
+    def p1_2(ui):
+        print("ouo")
+        ui.t3_1_angle.setText("OuO")
+
+    @staticmethod
+    def p1_3(ui):
+        print("ouo")
+        ui.t3_1_angle.setText("OuO")
+
+    @staticmethod
+    def p1_4(ui):
+        print("ouo")
+        ui.t3_1_angle.setText("OuO")
