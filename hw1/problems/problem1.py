0f4a726e35b3dbefdc29250f4f5ccbbe0411254c
aben20807 <aben20807@gmail.com>
2019-11-01 09:41:30 +0800

    Remove the useless code to more clear

--- hw1/problems/problem1.py
+++ hw1/problems/problem1.py
@@ -6,27 +6,19 @@ def close_all():
         cv2.destroyAllWindows()
 
 def p1_1(ui):
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
     (chessboard_height, chessboard_width) = (11, 8)
-    objp = np.zeros((chessboard_height*chessboard_width,3), np.float32)
-    objp[:,:2] = np.mgrid[0:chessboard_width,0:chessboard_height].T.reshape(-1,2)
-    objpoints = [] # 3d point in real world space
-    imgpoints = [] # 2d points in image plane.
+    img_dir = os.getcwd() + os.sep + "images" + os.sep + "images" + os.sep + "CameraCalibration" + os.sep
     for i in range(1, 16):
-        filename = os.getcwd() + os.sep + "images" + os.sep + "images" + os.sep + "CameraCalibration" + os.sep + str(i) + ".bmp"
-        img = cv2.imread(filename)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(gray, (chessboard_height,chessboard_width), None)
+        img = cv2.imread(filename=img_dir + str(i) + ".bmp")
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray_img, (chessboard_height,chessboard_width))
         if ret == True:
-            objpoints.append(objp)
-            corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-            imgpoints.append(corners2)
-
+            # Used to increase the accuracy of the points
+            corners = cv2.cornerSubPix(gray_img, corners, (11,11), (-1,-1), criteria)
             # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, (chessboard_height,chessboard_width), corners2,ret)
-            img = cv2.resize(img, (960,540))
-            cv2.imshow("img" + str(i), img)
+            img = cv2.drawChessboardCorners(img, (chessboard_height,chessboard_width), corners, ret)
+            cv2.imshow(str(i)+".bmp", img)
 
 def p1_2(ui):
     print("ouo")
