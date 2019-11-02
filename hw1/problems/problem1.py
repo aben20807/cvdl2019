d9b680fa079d1bb5c8e373cc61fb02ac00774a99
aben20807 <aben20807@gmail.com>
2019-11-02 11:49:20 +0800

    Finish 1.2

--- hw1/problems/problem1.py
+++ hw1/problems/problem1.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 def close_all_cv():
         cv2.destroyAllWindows()
@@ -20,8 +21,29 @@ def p1_1(ui):
             cv2.imshow(str(i)+".bmp", img)
 
 def p1_2(ui):
-    print("ouo")
-    ui.t3_1_angle.setText("OuO")
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+    (chessboard_height, chessboard_width) = (11, 8)
+    objp = np.zeros((chessboard_height*chessboard_width,3), np.float32)
+    objp[:,:2] = np.mgrid[0:chessboard_height,0:chessboard_width].T.reshape(-1,2)
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    
+    img_dir = os.getcwd() + os.sep + "images" + os.sep + "images" + os.sep + "CameraCalibration" + os.sep
+    for i in range(1, 16):
+        img = cv2.imread(filename=img_dir + str(i) + ".bmp")
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray_img, (chessboard_height,chessboard_width))
+        if ret == True:
+            
+            # Used to increase the accuracy of the points
+            acc_corners = cv2.cornerSubPix(gray_img, corners, (11,11), (-1,-1), criteria)
+            
+            objpoints.append(objp)
+            imgpoints.append(acc_corners)
+            _, cameraMatrix, _, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray_img.shape[::-1], None, None)
+            np.set_printoptions(formatter={'float': '{:.6f}'.format})
+            print(str(i) + ".bmp")
+            print(cameraMatrix)
 
 def p1_3(ui):
     print("ouo")
