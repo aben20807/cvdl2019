cffb98f6c28ce46e9231a8bb7802c98e885bdba8
aben20807 <aben20807@gmail.com>
2019-11-02 14:50:29 +0800

    Reuse the function in problem 1

--- hw1/problems/problem2.py
+++ hw1/problems/problem2.py
@@ -2,41 +2,31 @@ import cv2
 import os
 import numpy as np
 
+from .problem1 import calibrateCameraFromChessboard
+
+(chessboard_height, chessboard_width) = (11, 8)
+pyramid = np.float32([[0,0,-2], [1,1,0], [1,-1,0], [-1,-1,0], [-1,1,0]]).reshape(-1,3)
+img_dir = os.getcwd() + os.sep + "images" + os.sep + "images" + os.sep + "CameraCalibration" + os.sep
+
 def p2_1(ui):
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-    (chessboard_height, chessboard_width) = (11, 8)
-    objp = np.zeros((chessboard_height*chessboard_width,3), np.float32)
-    objp[:,:2] = np.mgrid[0:chessboard_height,0:chessboard_width].T.reshape(-1,2)
-    objpoints = [] # 3d point in real world space
-    imgpoints = [] # 2d points in image plane.
-    pyramid = np.float32([[0,0,-2], [1,1,0], [1,-1,0], [-1,-1,0], [-1,1,0]]).reshape(-1,3)
-    
-    img_dir = os.getcwd() + os.sep + "images" + os.sep + "images" + os.sep + "CameraCalibration" + os.sep
-    img_list = []
+    drew_img_list = []
+
     for i in range(1, 6):
-        img = cv2.imread(filename=img_dir + str(i) + ".bmp")
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(gray_img, (chessboard_height,chessboard_width))
-        if ret == True:
-
-            # Used to increase the accuracy of the points
-            acc_corners = cv2.cornerSubPix(gray_img, corners, (11,11), (-1,-1), criteria)
-
-            objpoints.append(objp)
-            imgpoints.append(acc_corners)
-            _, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray_img.shape[::-1], None, None)
-            _, rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, acc_corners, cameraMatrix, distCoeffs)
-            imgpts, _ = cv2.projectPoints(pyramid, rvecs, tvecs, cameraMatrix, distCoeffs)
+        filename = img_dir + str(i) + ".bmp"
+        ret = calibrateCameraFromChessboard(filename)
+        if ret['patternWasFound'] == True:
+            _, rvecs, tvecs, _ = cv2.solvePnPRansac(ret['objp'], ret['corners'], ret['cameraMatrix'], ret['distCoeffs'])
+            imgpts, _ = cv2.projectPoints(pyramid, rvecs, tvecs, ret['cameraMatrix'], ret['distCoeffs'])
 
             # Draw and save it to the list.
             # Because there was a delay between the processing of the images,
             # storing the drew images and showing them in the next loop
             # were used to handle the delay.
-            img = draw_pyramid(img, imgpts)
-            img_list.append(img)
+            img = draw_pyramid(ret['image'], imgpts)
+            drew_img_list.append(img)
 
     # Show the stored images
-    for img in img_list:
+    for img in drew_img_list:
         cv2.imshow(str(i)+".bmp", img)
         # Show the image 0.5 seconds and close it
         cv2.waitKey(500)
