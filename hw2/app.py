e42aab9f2d0f0450d77b21833d44ec85910920d4
aben20807 <aben20807@gmail.com>
2019-12-31 09:33:36 +0800

    hw2 init

---
+++ hw2/app.py
@@ -0,0 +1,35 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QAction
+import os
+import sys
+from functools import partial
+import signal
+
+from problems import problem1
+from problems import problem2
+from problems import problem3
+
+class MainUi(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainUi, self).__init__()
+        ui_path = os.getcwd() + os.sep + "ui" + os.sep + "hw2.ui"
+        uic.loadUi(ui_path, self)
+        self._bind_buttons()
+
+        self.show()
+
+    def _bind_buttons(self):
+        bind_dict = {
+            'b1_1': problem1.p1_1,
+            'b2_1': problem2.p2_1,
+            'b3_1': problem3.p3_1,
+            'b3_2': problem3.p3_2,
+        }
+        for object_name, bind_function in bind_dict.items():
+            self.findChild(QtWidgets.QPushButton, object_name).clicked.connect(partial(bind_function, self))
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal.SIG_DFL) # Trap the Ctrl-C and terminate
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainUi()
+    sys.exit(app.exec_())
---
+++ hw2/problems/problem3.py
@@ -0,0 +1,9 @@
+def p3_1(ui):
+    """
+    """
+    pass
+
+def p3_2(ui):
+    """
+    """
+    pass
